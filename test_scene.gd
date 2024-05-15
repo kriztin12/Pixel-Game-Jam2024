84de97e21b76abfc1dbd38516221b9583f5f4405
kriztin12 <kriztinmargaret@gmail.com>
2024-05-16 00:48:29 +1000

    get player's name

--- test_scene.gd
+++ test_scene.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Dialogic.start('test')
+	Dialogic.start('start_scene')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
