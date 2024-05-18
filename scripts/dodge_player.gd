3234d5ad22cc3387c361e99a1fb6ff926d77b6fb
kriztin12 <kriztinmargaret@gmail.com>
2024-05-19 00:40:04 +1000

    switch scene for dodgeball scene

--- scripts/dodge_player.gd
+++ scripts/dodge_player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var lastLocation = Vector2.ZERO
+var dialogue = preload("res://test_scene.tscn").instantiate()
 
 func _process(delta):
 	# Update last location in every frame
@@ -11,3 +12,7 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 100
 	move_and_slide()
+
+
+func _on_area_2d_2_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	get_tree().change_scene_to_file("res://test_scene.tscn")
