74ae579ae1c49f9a716e4eb1df07c5c53b6f43cd
kriztin12 <kriztinmargaret@gmail.com>
2024-05-19 00:07:35 +1000

    dance scene prototype done

--- scripts/dance_scene.gd
+++ scripts/dance_scene.gd
@@ -4,6 +4,36 @@ const ArrowLeft = preload("res://scenes/move_arrow_left.tscn")
 const ArrowRight = preload("res://scenes/move_arrow_right.tscn")
 const ArrowUp = preload("res://scenes/move_arrow_up.tscn")
 
+var random = 0
+var RNG = RandomNumberGenerator.new()
+
 func _ready():
 	# resize screen
 	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_VIEWPORT
+
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	$Timer.start()
+	RNG.randomize()
+	var random_int = RNG.randi_range(0, 3)
+	random = random_int
+	
+	# random 1 - 3
+	if random == 1:
+		var al = ArrowLeft.instantiate()
+		get_parent().add_child(al)
+		al.position = $Create/Marker2D_ArrowLeft.global_position
+		
+	if random == 2:
+		var ar = ArrowRight.instantiate()
+		get_parent().add_child(ar)
+		ar.position = $Create/Marker2D_ArrowRight.global_position
+		
+	if random == 3:
+		var au = ArrowUp.instantiate()
+		get_parent().add_child(au)
+		au.position = $Create/Marker2D_ArrowUp.global_position
+	
