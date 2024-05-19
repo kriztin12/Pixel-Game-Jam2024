3d8610e28b0e53544033c1fef6dad4729c9af958
Natasha-Nguyen <natasha-nguyen@bigpond.com>
2024-05-20 03:19:46 +1000

    added enter school scene

---
+++ scripts/enter_school.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().root.content_scale_factor = 2
+	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_DISABLED
+
+	Dialogic.start('enter_school')
