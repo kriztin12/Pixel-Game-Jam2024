a31642480612ed856177677f54d8f700e4d13780
Natasha-Nguyen <natasha-nguyen@bigpond.com>
2024-05-20 02:13:23 +1000

    created prologue scene
    also adjusted some text style settings

--- scripts/prologue.gd
+++ scripts/prologue.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().root.content_scale_factor = 2
+	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_DISABLED
+
+	Dialogic.start('prologue')
+	
