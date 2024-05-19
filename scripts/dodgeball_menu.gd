90537e4e41c50632206648524a73650121ee3671
kriztin12 <kriztinmargaret@gmail.com>
2024-05-19 22:50:02 +1000

    dialogue dodgeball

--- scripts/dodgeball_menu.gd
+++ scripts/dodgeball_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	get_tree().root.content_scale_factor = 3
+	get_tree().root.content_scale_factor = 4
 	get_tree().root.content_scale_mode = Window.CONTENT_SCALE_MODE_DISABLED
 
 func _on_button_pressed():
